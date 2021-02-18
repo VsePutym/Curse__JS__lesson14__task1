4351a9a0647afabcf5db6ed7a888dec74f03aec2
Vse Putyom <bertraid777@gmail.com>
2021-02-18 15:16:10 +0300

    fix(script.js): delete coment

--- js/script.js
+++ js/script.js
@@ -1,80 +1,42 @@
 'use strict';
 
-
-// function DomElement(selector, height, width, bg, fontSize) {
-//     this.selector = selector;
-//     this.height = height;
-//     this.width = width;
-//     this.bg = bg;
-//     this.fontSize = fontSize;
-//     console.log(this);
-// }
-
-// DomElement.prototype.createElement = function () {
-//     if (this.selector.slice(0, 1) === '.') {
-//         let text = document.createElement('div');
-//         text.classList.add('someText');
-//         text.textContent = 'some text lies here';
-//         document.body.insertAdjacentElement('afterbegin', text);
-//     } else if (this.selector[0] === '#') {
-//         document.body.insertAdjacentHTML('beforeend', '<p id="someParagraph">some Paragraph lies here</p>');
-//     }
-//     console.log(this);
-// };
-
-// const divText = new DomElement('.test1', '150px', '800px', '#5a18c4', '100px');
-// const paragraphText = new DomElement('#test2', '250px', '800px', '#8c469e', '100px');
-
-// divText.createElement();
-// paragraphText.createElement();
-
-// document.querySelector('.someText').style.cssText = `
-// height: ${divText.height}; 
-// width: ${divText.width}; 
-// background: ${divText.bg}; 
-// font-size: ${divText.fontSize}`;
-
-// document.getElementById('someParagraph').style.cssText = `
-// height: ${paragraphText.height}; 
-// width: ${paragraphText.width}; 
-// background: ${paragraphText.bg}; 
-// font-size: ${paragraphText.fontSize}`;
-
-
-function DomElement(selector, height, width, bg, fontSize){
+function DomElement(selector, height, width, bg, fontSize) {
     this.selector = selector;
-    this.height = height; 
-    this.width = width; 
+    this.height = height;
+    this.width = width;
     this.bg = bg;
     this.fontSize = fontSize;
+    console.log(this);
 }
 
-DomElement.prototype.addNewText = function(){
-    if(this.selector[0] === '.'){
+DomElement.prototype.createElement = function () {
+    if (this.selector.slice(0, 1) === '.') {
         let text = document.createElement('div');
         text.classList.add('someText');
         text.textContent = 'some text lies here';
         document.body.insertAdjacentElement('afterbegin', text);
-    }else if(this.selector[0] === '#'){
-        document.body.insertAdjacentHTML('beforeend','<p id="someParagraph">some Paragraph lies here</p>');
+    } else if (this.selector[0] === '#') {
+        document.body.insertAdjacentHTML('beforeend', '<p id="someParagraph">some Paragraph lies here</p>');
     }
+    console.log(this);
 };
 
-const textDiv = new DomElement('.test1', '150px', '800px', '#5a18c4', '100px');
+const divText = new DomElement('.test1', '150px', '800px', '#5a18c4', '100px');
 const paragraphText = new DomElement('#test2', '250px', '800px', '#8c469e', '100px');
 
-textDiv.addNewText();
-paragraphText.addNewText();
+divText.createElement();
+paragraphText.createElement();
 
 document.querySelector('.someText').style.cssText = `
-height: ${textDiv.height};
-width: ${textDiv.width};
-background-color: ${textDiv.bg};
-font-size: ${textDiv.fontSize}`;
+height: ${divText.height}; 
+width: ${divText.width}; 
+background: ${divText.bg}; 
+font-size: ${divText.fontSize}`;
 
 document.getElementById('someParagraph').style.cssText = `
-height: ${paragraphText.height};
-width: ${paragraphText.width};
-background-color: ${paragraphText.bg};
+height: ${paragraphText.height}; 
+width: ${paragraphText.width}; 
+background: ${paragraphText.bg}; 
 font-size: ${paragraphText.fontSize}`;
+
 
